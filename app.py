41857e1b044fcdf74303330bb1eae6321e9bbdda
versach <dan2004degvl@mail.ru>
2023-11-19 04:56:57 +1000

    v5 Added the start page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, flash, url_for, make_response
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 import os
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '314'
@@ -9,7 +12,6 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'users'
-
     username = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
@@ -20,9 +22,20 @@ class User(db.Model):
     is_master = db.Column(db.Boolean, default=False)
     is_customer = db.Column(db.Boolean, default=False)
     uid = db.Column(db.Integer, primary_key=True)
-
     def __repr__(self):
         return f'<User {self.username}>'
+    
+class ServiceType(db.Model):
+    __tablename__ = 'service_type'
+
+    sid = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name_service = db.Column(db.Text(collation='pg_catalog."default"'))
+    image = db.Column(db.LargeBinary)
+    subgroup = db.Column(db.Text(collation='pg_catalog."default"'))
+
+    def __repr__(self):
+        return f"<ServiceType(name_service='{self.name_service}', subgroup={self.subgroup}, image={self.image})>"
+
 
 
 # Создание таблицы в базе данных
@@ -36,8 +49,6 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         remember = request.form.get('remember')  # Получаем значение чекбокса
-
-        # print(user.email)
         
         if user.username == username and user.password == password:
             response = make_response()
@@ -45,20 +56,77 @@ def login():
             if remember:
                 response.set_cookie('user_token', 'aergergberbbdb', max_age=30 * 24 * 60 * 60)  # Например, токен действителен 30 дней
 
-            session['user_id'] = user.uid
+            session['uid'] = user.uid
+            session['username'] = user.username
+            session['email'] = user.email
             session['first_name'] = user.first_name
             session['last_name'] = user.last_name
             session['patronymic'] = user.patronymic
+            session['is_master'] = user.is_master
+            session['is_admin'] = user.is_admin
+            
 
             return redirect(url_for('home'))
         else:
             flash('Неверный логин или пароль. Пожалуйста, проверьте свои данные и повторите попытку.', 'danger')
     return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    user = User.query.filter_by(username=session['username']).first()
+    
+    # Извлекаем данные из базы данных
+    services_data = ServiceType.query.all()
+    
+    # Группируем данные по name_service
+    grouped_services = {}
+    for service in services_data:
+        if service.name_service not in grouped_services:
+            grouped_services[service.name_service] = []
+        grouped_services[service.name_service].append(service)
+        print(grouped_services)
+
+    # return render_template('home.html', grouped_services=grouped_services)
+    return render_template('home.html', user = user, grouped_services=grouped_services)
+
+
+@app.route('/service_details/<name_service>')
+def service_details_by_name(name_service):
+    # Здесь вы можете извлечь дополнительные данные для конкретного name_service
+    return render_template('masters_list.html', name_service=name_service)
+
+@app.route('/service_details/<subgroup>')
+def service_details_by_subgroup(subgroup):
+    # Здесь вы можете извлечь дополнительные данные для конкретного subgroup
+    return render_template('masters_list.html', subgroup=subgroup)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form.get('search_query')
+        print(query)
+        # Выполните поиск в базе данных
+        results = ServiceType.query.filter(
+            or_(
+                ServiceType.name_service.ilike(f'%{query}%'),
+                ServiceType.subgroup.ilike(f'%{query}%')
+            )
+        ).all()
+        print(results)
+        return render_template('search_results.html', results=results)
+    return render_template('search.html')
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('login.html')
+
         
 
 # new_item = User(username="Versach", email="dan2004degvl@mail.ru", last_name="Дегтярев", first_name="Данил", patronymic="Викторович", password="admin")
